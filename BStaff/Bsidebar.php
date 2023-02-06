4d101d619f5f3478e7091471f7916fe0592d3e74
Nowlly123 <jadesumalinog20@gmail.com>
2023-02-06 11:21:55 +0800

    Sectroy

--- BStaff/Bsidebar.php
+++ BStaff/Bsidebar.php
@@ -4,7 +4,7 @@
         <!-- Sidebar - Brand -->
         <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
             <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-laugh-wink"></i>
+                <i class=""></i>
             </div>
             <div class="sidebar-brand-text mx-3">Sectory</div>
         </a>
@@ -46,14 +46,14 @@
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                 aria-expanded="true" aria-controls="collapseUtilities">
                 <i class="fas fa-fw fa-wrench"></i>
-                <span>Mannage Accounts</span>
+                <span>Mannage</span>
             </a>
             <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                 data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
                     <h6 class="collapse-header"></h6>
-                    <a class="collapse-item" href="utilities-border.html">Resident</a>
-                    <a class="collapse-item" href="utilities-animation.html">Household</a>
+                    <a class="collapse-item" href="utilities-border.html">List of Resident</a>
+                    <a class="collapse-item" href="utilities-animation.html">List of Household</a>
                     <a class="collapse-item" href="utilities-other.html">Other</a>
                 </div>
             </div>
